3061716c3a7696d440278569882276a924e70d2e
Joffrey Demetz <joffrey.demetz@gmail.com>
2025-12-07 14:24:58 +0100

    Added Frequency enum .. updated PHP requirements

---
+++ src/Frequency.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author    Joffrey Demetz <joffrey.demetz@gmail.com>
+ * @license   MIT License; <https://opensource.org/licenses/MIT>
+ */
+
+namespace JDZ\Sitemap;
+
+enum Frequency: string
+{
+    case ALWAYS = 'always';
+    case HOURLY = 'hourly';
+    case DAILY = 'daily';
+    case WEEKLY = 'weekly';
+    case MONTHLY = 'monthly';
+    case YEARLY = 'yearly';
+    case NEVER = 'never';
+}
--- src/Url.php
+++ src/Url.php
@@ -8,9 +8,11 @@
 namespace JDZ\Sitemap;
 
 use JDZ\Sitemap\Exception;
+use JDZ\Sitemap\Frequency;
 
-class Url 
+class Url
 {
+  // Kept for backward compatibility
   public const ALWAYS = 'always';
   public const HOURLY = 'hourly';
   public const DAILY = 'daily';
@@ -18,57 +20,53 @@ class Url
   public const MONTHLY = 'monthly';
   public const YEARLY = 'yearly';
   public const NEVER = 'never';
-  
-  private const DEFAULT_FREQUENCY = self::WEEKLY;
+
   private const DEFAULT_PRIORITY = 0.5;
-  
-  private array $validFrequencies = [
-      self::ALWAYS,
-      self::HOURLY,
-      self::DAILY,
-      self::WEEKLY,
-      self::MONTHLY,
-      self::YEARLY,
-      self::NEVER,
-  ];
-  
+
   private string $loc;
   private string $lastmod;
-  private string $changefreq;
+  private Frequency $changefreq;
   private float $priority;
-  
-  public function __construct(string $loc, string $lastmod='now', string $changefreq=self::DEFAULT_FREQUENCY, float $priority=self::DEFAULT_PRIORITY)
+
+  public function __construct(string $loc, string $lastmod = 'now', Frequency|string $changefreq = Frequency::WEEKLY, float $priority = self::DEFAULT_PRIORITY)
   {
     $this->loc = $loc;
     $this->lastmod = (new \DateTime($lastmod, new \DateTimeZone('UTC')))->format('c');
-    $this->changefreq = $changefreq;
+
+    if (is_string($changefreq)) {
+      try {
+        $this->changefreq = Frequency::from($changefreq);
+      } catch (\ValueError $e) {
+        $validValues = array_map(fn($case) => $case->value, Frequency::cases());
+        throw new Exception('Please specify valid changeFrequency. Valid values are: ' . implode(', ', $validValues) . ' You have specified: ' . $changefreq);
+      }
+    } else {
+      $this->changefreq = $changefreq;
+    }
+
     $this->priority = $priority;
   }
-  
+
   /**
    * @throws  Exception
    */
   public function toSitemap(string $website): array
   {
     $location = \ltrim($this->loc, '/');
-    $location = $website.'/'.$location;
-    
-    if ( false === \filter_var($location, \FILTER_VALIDATE_URL) ){
-      throw new Exception('The location must be a valid URL. You have specified: '.$location);
+    $location = $website . '/' . $location;
+
+    if (false === \filter_var($location, \FILTER_VALIDATE_URL)) {
+      throw new Exception('The location must be a valid URL. You have specified: ' . $location);
     }
-    
-    if ( $this->priority < 0 || $this->priority > 1 ){
-      throw new Exception('Please specify valid priority. Valid values range from 0.0 to 1.0. You have specified: '.$this->priority);
+
+    if ($this->priority < 0 || $this->priority > 1) {
+      throw new Exception('Please specify valid priority. Valid values range from 0.0 to 1.0. You have specified: ' . $this->priority);
     }
-    
-    if ( !in_array($this->changefreq, $this->validFrequencies, true) ){
-      throw new Exception('Please specify valid changeFrequency. Valid values are: '.implode(', ', $this->validFrequencies).' You have specified: '.$this->changefreq);
-    }
-    
+
     return [
       'loc' => $location,
       'lastmod' => $this->lastmod,
-      'changefreq' => $this->changefreq,
+      'changefreq' => $this->changefreq->value,
       'priority' => $this->priority,
     ];
   }
